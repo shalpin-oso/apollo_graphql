b422bacf7923a65d44535e9d57faf7f417c09947
Sean Halpin <6930112+sean-halpin@users.noreply.github.com>
2023-06-28 12:11:55 +0000

    Updates

--- sinatra/search.rb
+++ sinatra/search.rb
@@ -3,15 +3,6 @@ require 'graphql'
 require 'json'
 require 'apollo-federation'
 require 'webrick'
-require 'webrick/https'
-
-# Configure SSL options
-ssl_options = {
-  SSLEnable: true,
-  SSLCertificate: './ssl/certificate.crt',
-  SSLPrivateKey: './ssl/private_key.key',
-  SSLCertName: [['CN', 'localhost']],
-}
 
 # Define your GraphQL schema
 class ProductType < GraphQL::Schema::Object
@@ -27,13 +18,6 @@ class ProductSearchInput < GraphQL::Schema::InputObject
   argument :query, String, required: true
 end
 
-class SortOrderInput < GraphQL::Schema::InputObject
-  include ApolloFederation::InputObject
-
-  argument :direction, String, required: true
-  argument :field, String, required: true
-end
-
 class QueryType < GraphQL::Schema::Object
   include ApolloFederation::Object
 
@@ -45,29 +29,35 @@ class QueryType < GraphQL::Schema::Object
     # Simulated logic to return a list of products based on the input
     [
       { id: 1, name: "Mug 1" },
-      { id: 2, name: "Mug 2" },
-      { id: 3, name: "Mug 3" }
+      { id: 2, name: "Mug 2" }
     ]
   end
 end
 
 class MySchema < GraphQL::Schema
   include ApolloFederation::Schema
-  include GraphQL::Introspection
+  federation version: '2.0'
+  use ApolloFederation::Tracing
 
   query QueryType
 end
 
 # Define a simple Sinatra app to handle the GraphQL requests
 class MyApp < Sinatra::Base
+  get '/graphql' do
+    p request
+  end
+
   post '/graphql' do
     request_payload = JSON.parse(request.body.read)
     result = MySchema.execute(request_payload['query'], variables: request_payload['variables'])
-    JSON.dump result
+    p result.to_json
   end
 end
 
-puts MySchema.to_json
+puts "*"*20
+puts MySchema.federation_sdl
+puts "*"*20
 # Run the Sinatra app
 MyApp.run!
 
